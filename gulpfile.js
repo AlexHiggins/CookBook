7934d2f7fd7557f0fa98456e32cb37c85b3f7461
AlexHiggins <alex.higgins@visualsoft.co.uk>
2014-10-25 16:42:21 +0100

    tidying up gulp file

--- gulpfile.js
+++ gulpfile.js
@@ -2,13 +2,5 @@ var elixir = require('laravel-elixir');
 
 elixir(function(mix) {
     mix.sass("app.scss")
-        .scripts([
-        "vendor/jquery-2.1.1.js",
-        "vendor/bootstrap-3.2.0.js",
-        "vendor/chosen.js",
-        "vendor/epiceditor.js",
-        "vendor/highlight.js",
-        "chosen.js",
-        "epiceditor.js"
-    ], 'resources/assets/js/');
+        .scripts([''], 'resources/assets/js/');
 });
