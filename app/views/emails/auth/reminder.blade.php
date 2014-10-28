90950df7ed981a9a87bb9a10bccf0d7c7b82dcdb
AlexHiggins <alex.higgins@visualsoft.co.uk>
2014-10-28 22:07:32 +0000

    page formatting funky

--- app/views/emails/auth/reminder.blade.php
+++ app/views/emails/auth/reminder.blade.php
@@ -1,26 +1,26 @@
 <!DOCTYPE html>
 <html lang="en">
 <head>
-  <meta charset="utf-8">
-  <style>
-		body {
-			font-family: "Helvetica Neue", Helvetica, Arial, sans-serif;
-      font-size: 14px;
-      line-height: 1.42857;
-			box-shadow: 0 1px 2px rgba(0, 0, 0, 0.15);
-			border-radius: 4px;
-			background-color: #fff;
-			padding: 15px;
-		}
+	<meta charset="utf-8">
+	<style>
+	body {
+		font-family: "Helvetica Neue", Helvetica, Arial, sans-serif;
+		font-size: 14px;
+		line-height: 1.42857;
+		box-shadow: 0 1px 2px rgba(0, 0, 0, 0.15);
+		border-radius: 4px;
+		background-color: #fff;
+		padding: 15px;
+	}
 
-		.page-title {
-			margin-top: 0;
-			margin-bottom: 15px;
-			color: #FB503B;
-			font-weight: 300;
-			font-size: 25px;
-    }
-  </style>
+	.page-title {
+		margin-top: 0;
+		margin-bottom: 15px;
+		color: #FB503B;
+		font-weight: 300;
+		font-size: 25px;
+	}
+	</style>
 </head>
 <body>
 
