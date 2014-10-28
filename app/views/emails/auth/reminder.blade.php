3f6f3c6ccdc8ef5bfcd23da24186b68638111ecb
AlexHiggins <alex.higgins@visualsoft.co.uk>
2014-10-28 22:05:13 +0000

    quick styling for email template

--- app/views/emails/auth/reminder.blade.php
+++ app/views/emails/auth/reminder.blade.php
@@ -1,16 +1,37 @@
 <!DOCTYPE html>
-<html lang="en-US">
-	<head>
-		<meta charset="utf-8">
-	</head>
-	<body>
-		<h2>Password Reset</h2>
+<html lang="en">
+<head>
+  <meta charset="utf-8">
+  <style>
+		body {
+			font-family: "Helvetica Neue", Helvetica, Arial, sans-serif;
+      font-size: 14px;
+      line-height: 1.42857;
+			box-shadow: 0 1px 2px rgba(0, 0, 0, 0.15);
+			border-radius: 4px;
+			background-color: #fff;
+			padding: 15px;
+		}
 
-		<div>
-			To reset your password, complete this form: {{ url('password/reset', [$token]) }}.<br/>
+		.page-title {
+			margin-top: 0;
+			margin-bottom: 15px;
+			color: #FB503B;
+			font-weight: 300;
+			font-size: 25px;
+    }
+  </style>
+</head>
+<body>
 
-			This link will expire in {{ config::get('auth.reminder.expire', 60) }} minutes.
-		</div>
-	</body>
+<div>
+	<h2 class="page-title">Password Reset</h2>
+	<div>
+		<p>To reset your password, complete this form: {{ url('password/reset', [$token]) }} </p>
+
+		<p>This link will expire in {{ config::get('auth.reminder.expire', 60) }} minutes. </p>
+	</div>
+</div>
+
+</body>
 </html>
-<? exit;?>
