c3827923c21dba80ef2dfd1bb477737eb955a6ad
AlexHiggins <alex.higgins@visualsoft.co.uk>
2014-11-14 14:24:23 +0000

    compression works again :)

--- gulpfile.js
+++ gulpfile.js
@@ -2,5 +2,13 @@ var elixir = require('laravel-elixir');
 
 elixir(function(mix) {
     mix.sass("app.scss")
-        .scripts([''], 'resources/assets/js/');
+        .scripts([
+            "vendor/jquery-2.1.1.js",
+            "vendor/bootstrap-3.2.0.js",
+            "vendor/chosen.js",
+            "vendor/epiceditor.js",
+            "vendor/highlight.js",
+            "chosen.js",
+            "epiceditor.js"
+        ], 'resources/assets/js/');
 });
